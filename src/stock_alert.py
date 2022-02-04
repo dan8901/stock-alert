cd4c4ab8d0551f486ad2aa7bd9b749cf53c761e0
Dan <nissim.dan@gmail.com>
2022-02-05 01:24:21 +0200

    finished first version

--- src/stock_alert.py
+++ src/stock_alert.py
@@ -7,11 +7,24 @@ import typing
 
 import httpx
 import pydantic
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+import jinja2
+import datetime
 
 FMP_API_URL = 'https://financialmodelingprep.com/api/v3'
 API_KEY_PARAM = {'apikey': os.environ['API_KEY']}
+GMAIL_PASSWORD = os.environ['GMAIL_PASSWORD']
 CONFIG_PATH = pathlib.Path('./src/config.json')
-
+SMTP_PORT = 465
+GMAIL_SMPT_SERVER_ADDRESS = 'smtp.gmail.com'
+APP_EMAIL_ADDRESS = 'thestockalertapp@gmail.com'
+PRICE_DROP_PERCENTAGE_THRESHOLD = 0.8
+LOGO_PATH = pathlib.Path('./images/logo.png')
+EMAIL_SUBJECT = 'Stock price alert'
+HTML_PATH = pathlib.Path('./src/index.html')
+READABLE_DATETIME_FORMAT = '%B %d, %Y, %H:%M'
 
 class Quote(pydantic.BaseModel):
     symbol: str
@@ -22,6 +35,7 @@ class Quote(pydantic.BaseModel):
 
 
 class ContactInfo(pydantic.BaseModel):
+    name: str
     email: str
 
 
@@ -32,13 +46,10 @@ class Config(pydantic.BaseModel):
 
 async def stock_alert():
     config = _load_config()
-    # quotes = await _get_all_quotes(config.tickers)
-    # for quote in quotes:
-    # if _should_notify(quote):
-    #     await _notify(quote, config.contact_info)
-    if True:
-        _send_email('bla', 'bla')
-
+    quotes = await _get_all_quotes(config.tickers)
+    for quote in quotes:
+        if _should_notify(quote):
+            _notify(quote, config.contact_info)
 
 def _load_config() -> Config:
     return Config.parse_obj(json.loads(CONFIG_PATH.read_text()))
@@ -54,20 +65,38 @@ async def _get_all_quotes(tickers: typing.List[str]) -> typing.List[Quote]:
     return quotes
 
 
-async def _should_notify(quote: Quote) -> bool:
-    return quote.price <= 0.8 * quote.priceAvg200
+def _should_notify(quote: Quote) -> bool:
+    return quote.price <= PRICE_DROP_PERCENTAGE_THRESHOLD * quote.priceAvg200
 
 
-async def _notify(quote: Quote, contact_info: ContactInfo):
-    pass
+def _notify(quote: Quote, contact_info: ContactInfo):
+    html = _generate_html(contact_info.name, quote)
+    _send_email(contact_info.email, html)
 
 
-def _send_email(email_address: str, content: str):
+def _generate_html(name: str, quote: Quote) -> str:
+    unformatted_html = HTML_PATH.read_text()
+    html_data = {
+        'name': name,
+        'symbol': quote.symbol,
+        'stock_name': quote.name,
+        'datetime': datetime.datetime.fromtimestamp(quote.timestamp).strftime(READABLE_DATETIME_FORMAT),
+        'price': quote.price,
+        'price_200_day_avg': quote.priceAvg200
+    }
+    return jinja2.Template(unformatted_html).render(html_data)
+
+def _send_email(email_address: str, html: str):
+    message = MIMEMultipart()
+    message['Subject'] = EMAIL_SUBJECT
+    message['From'] = APP_EMAIL_ADDRESS
+    message['To'] = email_address
+    image_part = MIMEImage(LOGO_PATH.read_bytes())
+    image_part.add_header('Content-ID', '<0>')
+    message.attach(image_part)
+    html_part = MIMEText(html, 'html')
+    message.attach(html_part)
     context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp_connection:
-        smtp_connection.login("thestockalertapp@gmail.com", 'Dann12345!')
-        smtp_connection.sendmail(
-            'thestockalertapp@gmail.com', 'thestockalertapp@gmail.com', """
-                                 Subject: Stock Alert
-                                 
-                                 Notice! this is an alert""")
+    with smtplib.SMTP_SSL(GMAIL_SMPT_SERVER_ADDRESS, SMTP_PORT, context=context) as smtp_connection:
+        smtp_connection.login(APP_EMAIL_ADDRESS, GMAIL_PASSWORD)
+        smtp_connection.sendmail(APP_EMAIL_ADDRESS, email_address, message.as_string())
